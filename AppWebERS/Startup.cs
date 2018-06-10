16e0552604061d5fabaf6647d093bc5abae81410
César Bravo <cebravo94@gmail.com>
2018-06-09 21:48:34 -0400

    Agregar archivos de proyecto.

---
+++ AppWebERS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppWebERS.Startup))]
+namespace AppWebERS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

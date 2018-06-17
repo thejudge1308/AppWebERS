82d2f96946fff8bdfc8c272ad37ec68592d38400
Unknown <diego.iturriaga.p@gmail.com>
2018-06-17 16:55:22 -0400

    Clase LoginController
    Clase controlador del login con la estructura base.

---
+++ AppWebERS/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace AppWebERS.Controllers
+{
+    public class LoginController : Controller
+    {
+        private String NombreUsuario;
+        private String Contrasenna;
+
+        public LoginController(string nombreUsuario, string contrasenna)
+        {
+            NombreUsuario = nombreUsuario;
+            Contrasenna = contrasenna;
+        }
+
+        
+    }
+}

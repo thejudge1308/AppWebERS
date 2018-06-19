deae9515c76575d21a03aa1035613d0fea16c691
Unknown <jnfco_18@hotmail.com>
2018-06-19 17:45:14 -0400

    Creacion de controlador para modificar cuenta (vacío)

---
+++ AppWebERS/Controllers/ModificarCuentaController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWebERS.Controllers
+{
+    public class ModificarCuentaController
+    {
+    }
+}

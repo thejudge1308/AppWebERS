fde44e18914d23f574b85bd71008ab886c717a9f
Diego Matus <dmatus15@alumnos.utalca.cl>
2018-06-19 12:24:02 -0400

    Vista modificacion de cuenta de usuario
    -Se creo un controlador provisorio UsuarioController
    -Se creo un AcciontResul ListarUsuarios dentro del controlador Usuario.
    -Se creo _ModalPartial en View/Usuario
    -Se creo _AlertaDeshabilitacionPartial en View/Usuario
    -Se creo vista listarUsarios en View/Usuario

---
+++ AppWebERS/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppWebERS.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: ModificacionUsuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListarUsuarios()
+        {
+            return View();
+        }
+
+
+    }
+}

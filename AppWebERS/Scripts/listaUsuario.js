5e2186ebaa7c9c9f80dca776bd1ca6dfd0973c88
Einherjar <capitan_ro@hotmail.com>
2018-06-20 14:11:47 -0400

    Se agrego funcionalidad con js para mostrar la alerta
    - Se agrego la alerta exitosa cuando se presiona el botón deshabilitar en la lista(js)
    - Se elimino una clase navbar de estilos que estaba duplicada en la etiqueta nav en layout.
    - Se modifico la vista AlertaDeshabilitacionPartial para poder obtener y modificar sus elementos desde js.
    - Se modifico ListarUsuarios.cshtml para adaptarla para el uso desde js
    - Se modifico el BundleConfig para que cuando agregara StyleRender de bootstrap agregue el script de popper que permite la funcionalidad de los dropdownsButton

---
+++ AppWebERS/Scripts/listaUsuario.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(() => {
+    let alerta = $('#alert');
+    cerrarAlertaAutomaticamente(); //Si hay una alerta la ocultara 
+
+    /**
+     * Evento sobre los botones deshabilitar, muestra la alerta.
+     */
+    $('.deshabilitar').on('click', () => {
+        alerta.toggleClass(function () {
+            console.log($(this));
+            if ($(this).hasClass('d-none')) {
+
+                $(this).addClass('d-flex').removeClass('d-none');
+
+                //Activa el timer para cerrar automaticamente la alerta al cabo de 2 segundos
+                cerrarAlertaAutomaticamente();
+            }
+            return $(this);
+        })
+    });
+
+    /**
+    * Funcion que cerrara una alerta que este siendo mostrada al cabo de 2 segundos.
+    */
+    function cerrarAlertaAutomaticamente() {
+        //Realiza una tarea asincrona que ocultara la alerta al cabo de 2 segundos
+        setTimeout(function () {
+            ocultarAlerta();
+        }, 2000);
+    }
+
+    /**
+     *  Funcion que oculta la alerta, usa un efecto de jquery
+     */
+    function ocultarAlerta() {
+        if (!$(this).hasClass('d-none')) {
+            alerta.slideUp("slow", function () {
+                alerta.toggleClass(function () {
+                        //Oculta la alert agregando la clase d-none 
+                        $(this).addClass('d-none').removeClass('d-flex');
+                    return $(this);
+                });
+                //Remueve el estilo dado por el efecto jquery
+                alerta.removeAttr('style');
+            });
+        }
+    }
+
+    /**
+     * Evento sobre el boton de la alerta para ocultarla.
+     */
+    $('#button-alert').on('click', () => {
+        ocultarAlerta();
+    })
+});

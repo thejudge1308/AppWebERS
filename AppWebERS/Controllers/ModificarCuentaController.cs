2cfc154f7b9a365fafad0e7c3cbc4722b7611e6e
Unknown <jnfco_18@hotmail.com>
2018-06-19 21:44:57 -0400

    Revert "Creado el metodo post , get y modificar usuario"
    This reverts commit faebc5a28a75db1e7f3471ca9ec80f9466b840b2.

---
+++ AppWebERS/Controllers/ModificarCuentaController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWebERS.Controllers
+{
+    public class ModificarCuentaController : Controller
+    {
+
+        [HttPost]
+        public ActionResult modificarCuenta
+
+    }
+}

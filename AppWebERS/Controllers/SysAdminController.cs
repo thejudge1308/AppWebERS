05c7a5c358c2677140737eb1ad9143400e3ab90e
raimundovu <rvasquez15@alumnos.utalca.cl>
2018-06-25 16:42:30 -0400

    Modificacion SysAdminController
    Se crean los metodos para instanciar el nuevo usuario y encriptar su clave, falta la conexion a la base de datos para hacer verificaciones de valores repetidos y demas

--- AppWebERS/Controllers/SysAdminController.cs
+++ AppWebERS/Controllers/SysAdminController.cs
@@ -1,7 +1,10 @@
 ﻿using AppWebERS.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,33 +13,77 @@ namespace AppWebERS.Controllers
     public class SysAdminController : Controller
     {
         // GET: SysAdmin
+
+
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public void ListarProyectos() {
+        public void ListarProyectos()
+        {
 
         }
 
-        public void ListarUsuarios() {
+        public void ListarUsuarios()
+        {
 
         }
 
-        public void CrearProyecto() {
+        public void CrearProyecto()
+        {
 
         }
 
-        public ActionResult CrearUsuario() {
-            return View();
+        [HttpGet]
+        public ActionResult CrearUsuario()
+        {
+
+            RegisterViewModel modeloUsuario = new RegisterViewModel();
+
+            return View(modeloUsuario);
         }
 
-        public void VerDetalleDelProyecto(Proyecto proyecto) {
+        [HttpPost]
+        public ActionResult CrearUsuario(RegisterViewModel modeloUsuario)
+        {
+
+            ModelState.Clear();
+            string rut = modeloUsuario.Rut;
+            string nombre = modeloUsuario.Nombre;
+            string correo = modeloUsuario.Email;
+            string contrasena = modeloUsuario.ConfirmPassword;
+            contrasena = this.encriptarClave(contrasena);
+            Usuario nuevoUsuario = new Usuario(rut, nombre, correo, contrasena, "Default");
+            
+            nuevoUsuario.Crear();
+
+            ViewBag.SuccessMessage = "Registro exitoso";
+            return View("CrearUsuario", new RegisterViewModel());
+        }
+
+
+
+        public void VerDetalleDelProyecto(Proyecto proyecto)
+        {
 
         }
 
-        public void AsignarJefeAProyecto(Proyecto proyecto, Usuario usuario) {
+        public void AsignarJefeAProyecto(Proyecto proyecto, Usuario usuario)
+        {
 
+        }
+
+        private string encriptarClave(string original) {
+
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                UTF8Encoding utf8 = new UTF8Encoding();
+                byte[] data = md5.ComputeHash(utf8.GetBytes(original));
+                return Convert.ToBase64String(data);
+            }
         }
     }
 }
+ 

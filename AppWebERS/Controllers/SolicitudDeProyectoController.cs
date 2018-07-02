4078af82da2cc229f39a6fba29ccdf9c08be1a78
Unknown <jose.nuez96@gmail.com>
2018-07-01 21:45:28 -0400

    Interfaz Listado de Solicitudes
    Se creo la interfaz para el listado de Solicitudes, falta hacer las query y el controlador

---
+++ AppWebERS/Controllers/SolicitudDeProyectoController.cs
@@ -0,0 +1,18 @@
+﻿using AppWebERS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppWebERS.Controllers
+{
+    public class SolicitudDeProyectoController : Controller
+    {
+        // GET: SolicitudDeProyecto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

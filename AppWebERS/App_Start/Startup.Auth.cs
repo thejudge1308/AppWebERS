47e05c0585e1711c4c0d55add081f3cbff798256
Einherjar <capitan_ro@hotmail.com>
2018-07-10 20:17:16 -0400

    Agregue modificarViewModel y modificacion loginPath startup.Auth
    - Se agrego el modificarViewModel a CuentaViewModel
    - Se cambio el login path en Startup.Auth.cs para que rediriga al controlador correcto al pedir autorizacion

--- AppWebERS/App_Start/Startup.Auth.cs
+++ AppWebERS/App_Start/Startup.Auth.cs
@@ -25,7 +25,7 @@ namespace AppWebERS
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
+                LoginPath = new PathString("/Cuenta/Login"),
                 Provider = new CookieAuthenticationProvider
                 {
                     // Enables the application to validate the security stamp when the user logs in.
--- AppWebERS/Models/CuentasViewModel.cs
+++ AppWebERS/Models/CuentasViewModel.cs
@@ -74,7 +74,32 @@ namespace AppWebERS.Models
         public string UserName { get; set; }
     }
 
-    public class ResetPasswordViewModel
+
+    public class ModificarViewModel
+    {
+        [RegularExpression("[0-9]*", ErrorMessage = "Rut no válido.")]
+        [StringLength(8, ErrorMessage = "El rut debe tener entre 7 a 8 caracteres (sin guión ni digito verif.)", MinimumLength = 7)]
+        [Display(Name = "Rut")]
+        public string Rut { get; set; }
+
+        [RegularExpression("([ ]?[a-zA-Z])*", ErrorMessage = "Nombre no válido.")]
+        [StringLength(50, ErrorMessage = "El largo del nombre deber ser entre 1 a 50 caracteres.", MinimumLength = 1)]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [StringLength(16, ErrorMessage = "La contraseña debe tener de 3 a 16 caracteres.", MinimumLength = 3)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        public string Tipo { get; set; }
+    }
+
+        public class ResetPasswordViewModel
     {
         [Required]
         [EmailAddress]

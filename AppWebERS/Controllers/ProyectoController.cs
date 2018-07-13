549854f480917873b2fc1e94285958662822db82
Gerardo Estrada <gerardoestrada14@hotmail.com>
2018-07-13 19:18:28 -0400

    Vista Usuarios de Proyecto
    Agregada vista para listar usuarios involucrados en un proyecto determinado.
    
    Kappa.

--- AppWebERS/Controllers/ProyectoController.cs
+++ AppWebERS/Controllers/ProyectoController.cs
@@ -32,6 +32,25 @@ namespace AppWebERS.Controllers
             return View();
         }
 
+
+        // GET: Proyecto/ListaUsuarios/5
+        public ActionResult ListaUsuarios(int id) {
+            Proyecto proyecto = this.GetProyecto(id);
+            string UsuarioActual = System.Web.HttpContext.Current.User.Identity.Name; // pregunta el usuario actual
+            //Debug.WriteLine("Usuario actual: " + UsuarioActual);
+            List<Usuario> usuarios = GetListaUsuarios(id);
+            ViewData["usuarios"] = TipoDePermiso(UsuarioActual);
+            ViewData["proyecto"] = proyecto;
+            return View();
+        }
+
+        // POST: Proyecto/Detalles/5
+        [HttpPost]
+        public ActionResult ListaUsuarios(FormCollection datos) {
+
+            return View();
+        }
+
         /**
         * Autor: Patricio Quezada
         * <param name = "id" > Id del proyecto.</param>
@@ -74,6 +93,52 @@ namespace AppWebERS.Controllers
         private int TipoDePermiso(String usuario) {
             //Esto estara completado una vez que este implementado el Entity Framework
             return Proyecto.AUTH_COMO_JEFE_DE_PROYECTO;
+        /**
+        * Autor: Gerardo Estrada
+        * <param name = "id" > Id del proyecto.</param>
+        * <returns>La lista de usuarios involucrados en el proyecto</returns>
+        * 
+        **/
+        private List<Usuario> GetListaUsuarios(int id) {
+            this.conexion = ConectorBD.Instance;
+
+            List<Usuario> listaUsuarios = new List<Usuario>();
+
+            string consulta = "SELECT users.Rut, users.Nombre, users.Email, users.Tipo FROM users, vinculo_usuario_proyecto, proyecto WHERE id_proyecto = " + id + " AND vinculo_usuario_proyecto.ref_proyecto = id_proyecto AND vinculo_usuario_proyecto.ref_usuario = users.Rut ;";
+            MySqlDataReader reader = this.conexion.RealizarConsulta(consulta);
+            if (reader == null) {
+                this.conexion.CerrarConexion();
+                return null;
+            }
+            else {
+                while (reader.Read()) {
+                    string rut = reader.GetString(0);
+                    string nombre = reader.GetString(1);
+                    string correo = reader.GetString(2);
+                    string tipo = reader.GetString(3);
+                    
+                    listaUsuarios.Add(new Usuario(rut, nombre, correo, tipo));
+                }
+
+                this.conexion.CerrarConexion();
+                return listaUsuarios;
+            }
+
+
+            //Proyecto proyecto = null;
+            //this.conexion = ConectorBD.Instance;
+            //string consulta = "SELECT users.Nombre, users.Rut, users.Email, users.Tipo FROM users, vinculo_usuario_proyecto, proyecto WHERE id_proyecto = " + id + " AND vinculo_usuario_proyecto.ref_proyecto = id_proyecto AND vinculo_usuario_proyecto.ref_usuario = users.Rut ;";
+            //MySqlDataReader data = this.conexion.RealizarConsulta(consulta);
+            //if (data != null) {
+            //    data.Read();
+            //    string nombre = data["nombre"].ToString();
+            //    string rut = data["rut"].ToString();
+            //    string correo_electronico = data["correo_electronico"].ToString();
+            //    string tipo = data["tipo"].ToString();
+
+            //    this.conexion.CerrarConexion();
+            //}
+            //return Usuario;
         }
 
     }

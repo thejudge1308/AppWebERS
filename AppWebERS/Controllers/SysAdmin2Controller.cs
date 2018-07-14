5b1752bbc09b80401043c80d2c561935f31f4f33
Unknown <marcelmarch95@gmail.com>
2018-07-14 05:50:12 -0400

    Layout Principal (Sysadmin, usuario)
    Version Beta

---
+++ AppWebERS/Controllers/SysAdmin2Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppWebERS.Controllers
+{
+    public class SysAdmin2Controller : Controller
+    {
+        // GET: SysAdmin2
+        public ActionResult Index()
+        {
+            return View("Principal");
+        }
+
+        // GET: SysAdmin2
+        public ActionResult Principal()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppWebERS/Controllers/UserViewController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppWebERS.Controllers
+{
+    public class UserViewController : Controller
+    {
+        // GET:  UserView
+        public ActionResult Index()
+        {
+            return View("Principal");
+        }
+
+        // GET: UserView
+        public ActionResult Principal()
+        {
+            return View();
+        }
+    }
+}

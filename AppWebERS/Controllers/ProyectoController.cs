b15b88dd41ad24be976795231d6adb3331eff6db
Unknown <jnfco_18@hotmail.com>
2018-07-14 20:02:46 -0400

    Revert "Merge remote-tracking branch 'origin/NirvanaFolder/feature/IdentityNico' into NirvanaFolder/feature/interfazUsuario"
    This reverts commit 9ae32a5e376cfec8ef49a33651082d8280a6e21d.

--- AppWebERS/Controllers/ProyectoController.cs
+++ AppWebERS/Controllers/ProyectoController.cs
@@ -6,16 +6,13 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data;
 using MySql.Data.MySqlClient;
-using Microsoft.AspNet.Identity;
 
 namespace AppWebERS.Models
 {
     public class ProyectoController : Controller
     {
         private ConectorBD Conector = ConectorBD.Instance;
-        protected MySQLDatabase MySQLDatabase { get; set; }
-        protected UserStore<Usuario> UserManager { get; set; }
-
+        List<NombreProyecto> listaProyectosNombres = new List<NombreProyecto>();
         // GET: Proyecto
         public ActionResult ListarProyectos()
         {
@@ -207,19 +204,26 @@ namespace AppWebERS.Models
          * Autor: Nicolás Hervias
          * Envía una solicitud para unirse al proyecto seleccionado (esta se guarda en la BD)
          * Parámetros: PosProyecto. Es la posición que tiene el proyecto en la lista de proyectos
-         */ 
-         [HttpPost]
+         */
+        [HttpPost]
         public void AgregarUsuarioAProyecto(int PosProyecto)
         {
-            this.MySQLDatabase = new MySQLDatabase();
-            this.UserManager = new UserStore<Usuario>(new UserStore<Usuario>(this.MySQLDatabase));
             List<int> ListaProyectos = ListaProyectosIds();
-            int IdProyectoAUnirse = ListaProyectos[PosProyecto]; 
+            int IdProyectoAUnirse = ListaProyectos[PosProyecto];
             string UsuarioSolicitante = System.Web.HttpContext.Current.User.Identity.Name; // obtiene el user logueado actualmente (rut)
+        }
 
-            string Values = "'" + IdProyectoAUnirse + "','" + UsuarioSolicitante + "'";
-            string Consulta = "INSERT INTO Solicitud_vinculacion (ref_proyecto,ref_solicitante) VALUES (" + Values + ");";
-            if (this.Conector.RealizarConsultaNoQuery(Consulta))
+        /*
+         * Autor Juan Abello
+         * Metodo encargado de obtener los nombres de los proyectos que existen ,guardarlos en una lista y retornar esta.
+         * <param void>
+         * <returns> listaProyectosNombres 
+         */
+        public List<NombreProyecto> ListaDeProyectos()
+        {
+            string consulta = "SELECT nombre FROM proyecto";
+            MySqlDataReader reader = this.Conector.RealizarConsulta(consulta);
+            if (reader == null)
             {
                 this.Conector.CerrarConexion();
                 return null;

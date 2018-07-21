ca89fcf1d023c5abec3371d7f4bde1eb9ef28b45
Diego <diego.iturriaga.p@gmail.com>
2018-07-21 00:06:46 -0400

    Metodos para Aceptar y Rechazar una solicitud
    Se implementaron los métodos necesarios para aceptar o rechazar una solicitud en particular.

--- AppWebERS/Models/SolicitudDeUsuario.cs
+++ AppWebERS/Models/SolicitudDeUsuario.cs
@@ -15,6 +15,29 @@ namespace AppWebERS.Models
 
         }
 
-       
+
+        public bool AceptarSolicitud(int idProyecto, string idUsuario)
+        {
+            string consultaUpdate = "UPDATE Solicitud_jefeproyecto_usuario SET estado = 1 WHERE ref_destinario='"+idUsuario+"' AND ref_proyecto="+idProyecto+" AND estado=0;";
+            if (this.conexion.RealizarConsultaNoQuery(consultaUpdate))
+            {
+                string consultaUpdate2 = "INSERT INTO vinculo_usuario_proyecto (ref_usuario,ref_proyecto,rol) VALUES ('"+idUsuario+"',"+idProyecto+",'USUARIO');";
+                if (this.conexion.RealizarConsultaNoQuery(consultaUpdate2))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        public bool RechazarSolicitud(int idProyecto, string idUsuario)
+        {
+            string consultaUpdate = "UPDATE Solicitud_jefeproyecto_usuario SET estado = 2 WHERE ref_destinario='" + idUsuario + "' AND ref_proyecto='" + idProyecto + "' AND estado=0;";
+            if (this.conexion.RealizarConsultaNoQuery(consultaUpdate))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }

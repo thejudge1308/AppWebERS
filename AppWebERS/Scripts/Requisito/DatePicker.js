53a1c3e5d88c551e4fb77ea122d9cec875b2468e
Patricio Quezada <the.judge16@gmail.com>
2018-07-27 00:44:57 -0400

    Implementacion de Datepicker en Requisitos
    Cambios: Requisito.cshtml, Layout.cshtml
    Agregado: DatePicker

---
+++ AppWebERS/Scripts/Requisito/DatePicker.js
@@ -0,0 +1,14 @@
+﻿// Creado por patricio Quezada
+//Implementación de datepicker y deshabilitar el textbox en escritura. 
+$(document).ready(function () {
+    $("#Fecha").keypress(function (e) {
+        return false
+    });
+    $.datepicker.setDefaults($.datepicker.regional["es"]);
+    $("#Fecha").datepicker({
+        dateFormat: 'yy-mm-dd',
+        showAnim:'clip',
+        changeMonth: true,
+        changeYear: true
+    })
+});

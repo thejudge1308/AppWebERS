425574835867ef258e8e90200c991abf4e0109de
Patricio Quezada <the.judge16@gmail.com>
2018-07-31 19:55:52 -0400

    Arreglos de adaptacion y optimizacion de codigo
    Se arreglaron los javascript

--- AppWebERS/Scripts/Proyecto/Referencias_modal.js
+++ AppWebERS/Scripts/Proyecto/Referencias_modal.js
@@ -1,9 +1,9 @@
 ﻿$(document).ready(function () {
-    
-    var elementos = 0;
+    getReferencias();
+    /*var elementos = 0;
     if (elementos == 0) {
         $('#referencia_table').find('tbody:last').append(filaVacia());
-    }
+    }*/
 });
 
 $("#ejemplo_m").click(function () {
@@ -41,7 +41,7 @@ $('#referencia_modal .modal-footer').on('click', '#guardar_libro', function () {
     if ($.trim($("#autores-box").val()) == "" || $.trim($("#anio-box").val()) == "" || $.trim($("#titulo-box").val()) == "" || $.trim($("#lugar-box").val()) == "" || $.trim($("#editorial-box").val())=="") {
         $("#mensaje").text("Ingrese todos los datos.");
     } else {
-        
+        guardarLibro($("#ProyectoActual_IdProyecto").val(), $.trim($("#autores-box").val()), $.trim($("#anio-box").val()), $.trim($("#titulo-box").val()), $.trim($("#lugar-box").val()), $.trim($("#editorial-box").val()));
     }
     
 });
@@ -52,18 +52,51 @@ $('#referencia_modal .modal-footer').on('click', '#guardar_paper', function () {
     if ($.trim($("#autores-box").val()) == "" || $.trim($("#anio-box").val()) == "" || $.trim($("#titulo-box").val()) == "" || $.trim($("#nombre-box").val()) == "" || $.trim($("#vol-box").val()) == "" || $.trim($("#pag-box").val()) == "") {
         $("#mensaje").text("Ingrese todos los datos.");
     } else {
-
+        guardarRevista($("#ProyectoActual_IdProyecto").val(), $.trim($("#autores-box").val()), $.trim($("#anio-box").val()), $.trim($("#titulo-box").val()), $.trim($("#nombre-box").val()), $.trim($("#vol-box").val()), $.trim($("#pag-box").val()));
     }
 });
 
 
 //Remove Fila actual
 $('#referencia_table').on('click', '#boton_fila', function () {
-    console.log($('#referencia_table tr').length);
-    $(this).closest('tr').remove();
-    if ($('#referencia_table tr').length == 1) {
-        $('#referencia_table').find('tbody:last').append(filaVacia());
-    }
+
+    var td = $(this).parent();
+    var tr = td.parent();
+    var value = tr.find("span").text();
+
+    var Referencia = {
+        id: $("#ProyectoActual_IdProyecto").val(),
+        valor: value
+    };
+    $.ajax({
+        type: "POST",
+        url: "/Proyecto/RemoverReferenciaPaper",
+        data: JSON.stringify(Referencia),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            //mostrarAlerta("Modificado con éxito.");
+            console.log(response);
+            if (response) {
+                mostrarAlerta("Eliminado con éxito");
+                tr.remove();
+                if ($('#referencia_table tr').length == 1) {
+                    $('#referencia_table').find('tbody:last').append(filaVacia());
+                }
+            }
+            else {
+                mostrarAlerta("No se ha podido eliminar la referencia.");
+            }
+        },
+        failure: function (response) {
+            mostrarAlerta(response.responseText);
+        },
+        error: function (response) {
+            mostrarAlerta(response.responseText);
+        }
+    });  
+
+
 });
 
 //Opciones del modal
@@ -105,7 +138,7 @@ function init_modal() {
         '</div >'+
         '<div class="row">'+
             '<div class="col-md-12 m-2">'+
-                '<button id="paper-button" type="button" style="width:100%" class="btn btn-primary">Peipa</button>'+
+                '<button id="paper-button" type="button" style="width:100%" class="btn btn-primary">Revista.</button>'+
             '</div>'+
         '</div>'+
         '</div >'+
@@ -183,10 +216,10 @@ function paper_modal() {
     return html;
 }
 
-function agregarFila() {
+function agregarFila(mensaje) {
     html =
         '<tr>' +
-        '<td>Ejemplo</td>' +
+        '<td class="mesaje"><span>' + mensaje + '</span></td>' +
         '<td class="text-right"><a class="btn btn-danger text-white" id="boton_fila" >Eliminar</a></td>' +
         '</tr>';
     return html;
@@ -205,14 +238,108 @@ function filaVacia() {
  */
 //Get
 function getReferencias() {
+    var urlget = "/Proyecto/MostrarReferencia/" + $("#ProyectoActual_IdProyecto").val();
+    $.get(urlget, function () {
+    })
+        .done(function (data) {
+            if (data == "null") {
+                $('#referencia_table').find('tbody:last').append(filaVacia());
+            } else {
+                console.log(data);
+                $.each(data, function (i, item) {
+                    console.log(item.valor);
+                    $("#referencia_table").find("tbody:last .not-found").remove();
+                    $('#referencia_table').find('tbody:last').append(agregarFila(item.valor));
+                    
+                });
 
+               
+            }
+
+        })
+        .fail(function () {
+            //alert("error");
+        })
+        .always(function () {
+            //alert("finished");
+        });
 }
 
 //Post
-function guardarLibro() {
-
+function guardarLibro(idR, autoresR, anioR, tituloR, lugarR, editorialR) {
+    var JsonReferenciaLibro = {
+        id: idR,
+        autores : autoresR,
+        anio: anioR,
+        titulo: tituloR,
+        lugar: lugarR,
+        editorial: editorialR
+    };
+    console.log(JsonReferenciaLibro);
+    $.ajax({
+        type: "POST",
+        url: "/Proyecto/AgregarReferenciaLibro",
+        data: JSON.stringify(JsonReferenciaLibro),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            //mostrarAlerta("Modificado con éxito.");
+            console.log(response);
+            if (response) {
+                mostrarAlerta("Agregado con éxito");
+                $('#referencia_modal').modal('toggle');
+                $('#referencia_table tbody').empty();
+                getReferencias();   
+            }
+            else {
+                mostrarAlerta("No se ha podido guardar la referencia.");
+            }
+            
+        },
+        failure: function (response) {
+            mostrarAlerta(response.responseText);
+        },
+        error: function (response) {
+            mostrarAlerta(response.responseText);
+        }
+    });  
 }
 
-function guardarRevista() {
+function guardarRevista(idR, autoresR, fechaR, tituloR, revistaR, volumenR, pagR) {
+    var JsonReferenciaPaper = {
+        id: idR,
+        autores: autoresR,
+        fecha: fechaR,    
+        titulo: tituloR,
+        revista: revistaR,
+        volumen: volumenR,
+        pag: pagR
+    };
 
+    $.ajax({
+        type: "POST",
+        url: "/Proyecto/AgregarReferenciaPaper",
+        data: JSON.stringify(JsonReferenciaPaper),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            //mostrarAlerta("Modificado con éxito.");
+            console.log(response);
+            if (response) {
+                mostrarAlerta("Agregado con éxito");
+                $('#referencia_modal').modal('toggle');
+                $('#referencia_table tbody').empty();
+                getReferencias();   
+            }
+            else {
+                mostrarAlerta("No se ha podido guardar la referencia.");
+            }
+        },
+        failure: function (response) {
+            mostrarAlerta(response.responseText);
+        },
+        error: function (response) {
+            mostrarAlerta(response.responseText);
+        }
+    });  
 }
